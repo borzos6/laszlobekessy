8ebd395d2a23549233d280bc9090fddc62d6aa40
László Békéssy <borzos6@gmail.com>
2024-03-14 23:13:44 +0200

    Codeberryschool.com app resources & infra initialized

---
+++ apps/codeberryschool-com/infra/main.tf
@@ -0,0 +1,35 @@
+# ==================================== #
+# PRODUCTION
+# ==================================== #
+module "codeberryschool_com_production" {
+  source = "../../../infrastructure/aws-blueprint-github-s3-cloudfront/src"
+
+  providers = {
+    aws.us_east_1    = aws.us_east_1
+    aws.eu_central_1 = aws.eu_central_1
+  }
+
+  # Domain setup
+  main_url = "codeberryschool.com"
+  desired_urls = [
+    "codeberryschool.com",
+    "www.codeberryschool.com",
+    "orange.codeberryschool.com",
+    "central.codeberryschool.com",
+    "data-mining.codeberryschool.com",
+    "stabill.codeberryschool.com",
+    "payment.codeberryschool.com",
+    "api.codeberryschool.com",
+    "design-system.codeberryschool.com",
+  ]
+  zone_name              = "codeberryschool.com"
+  acm_certificate_domain = "codeberryschool.com"
+
+  # Deployment setup
+  iam_deployment_user_name    = "laszlobekessy.deploy.website"
+  cloudfront_logs_bucket_name = "laszlobekessy-cloudfront-logs"
+}
+
+
+
+
---
+++ apps/codeberryschool-com/infra/outputs.tf
@@ -0,0 +1,3 @@
+output "cloudfront_distributions" {
+  value = module.codeberryschool_com_production.cloudfront_distributions
+}
